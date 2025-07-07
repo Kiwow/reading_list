07f9fd0a07555f96569a680c0dee6e8f9265dbda
Matyáš Volf <volfmatyas1@gmail.com>
2025-07-07 19:09:01 +0200

    Add some example links

--- src/lib/links.svelte.ts
+++ src/lib/links.svelte.ts
@@ -3,12 +3,52 @@
 
 export const unreadLinks = $state([
 	{
+		title: 'Figma - made by Evan',
+		href: 'https://madebyevan.com/figma/?utm_source=substack&utm_medium=email'
+	},
+	{
+		title: 'Shed the weight of an image by using the new AVIF format',
+		href: 'https://the-sustainable.dev/shed-the-weight-of-an-image-by-using-the-new-avif-format/'
+	},
+	{
+		title: 'What’s the worst colour for your digital carbon footprint?',
+		href: 'https://the-sustainable.dev/whats-the-worst-colour-for-your-digital-carbon-footprint/'
+	},
+	{
+		title: 'Quality is a trap',
+		href: 'https://ericwbailey.website/published/quality-is-a-trap/?utm_source=the-index&utm_medium=newsletter'
+	},
+	{
+		title: 'Vite 7.0 is out! | Vite',
+		href: 'https://vite.dev/blog/announcing-vite7'
+	},
+	{
+		title: 'Good practices from pixels to systems',
+		href: 'https://goodpractices.design/?utm_source=the-index&utm_medium=newsletter'
+	},
+	{
+		title: 'Live Content Collections: A Deep Dive | Astro',
+		href: 'https://astro.build/blog/live-content-collections-deep-dive/?utm_source=the-index&utm_medium=newsletter'
+	},
+	{
+		title: 'How to Keep Up With New CSS Features | CSS-Tricks',
+		href: 'https://css-tricks.com/how-to-keep-up-with-new-css-features/?utm_source=the-index&utm_medium=newsletter'
+	},
+	{
+		title: 'The GZIP Penalty. Sometimes compression is a bad idea | by Joe Honton | Medium',
+		href: 'https://joehonton.medium.com/the-gzip-penalty-d31bd697f1a2'
+	},
+	{
+		title: 'Atoms, molecules, organisms | Swizec Teller',
+		href: 'https://swizec.com/blog/atoms-molecules-organisms/'
+	},
+	{
 		title: 'Example article',
-		href: '#1'
+		href: 'https://developer.mozilla.org/en-US/'
 	},
 	{
 		title: 'Another article, this time with a much longer name—indeed, it is a long one',
-		href: '#2'
+		href: 'https://www.wikipedia.org/'
 	},
 	{
 		title: 'Yet another example article',
