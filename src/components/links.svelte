9d0704106b0af996ee683b1812274de69c1d53d8
Matyáš Volf <volfmatyas1@gmail.com>
2025-07-08 14:30:19 +0200

    Boom bapo

--- src/components/links.svelte
+++ src/components/links.svelte
@@ -9,16 +9,30 @@
 	};
 
 	let { links, isUnread, markAsOther, removeLink }: Props = $props();
+
+	function getLinkHost(href: string): string {
+		try {
+			return new URL(href).hostname;
+		} catch {
+			return '';
+		}
+	}
 </script>
 
 <ol role="list" class="flow links">
-  <!--
+	<!--
     TODO: when adding a link with the same href as one already present, an edge case is hit
-    and rendering of the list in links.svelte might break
+    and rendering of the list might break
   -->
 	{#each links as link, index (link.href)}
 		<li class="link">
-			<a href={link.href} rel="nofollow" class="link-title">{link.title}</a>
+			<a href={link.href} rel="nofollow" class="link-title">
+				{link.title}
+				<small class="link-domain-name">
+					<span class="visually-hidden">from</span>
+					{getLinkHost(link.href)}
+				</small>
+			</a>
 			<menu class="link-actions" role="list">
 				<li>
 					<button type="button" onclick={() => markAsOther(index)}>
