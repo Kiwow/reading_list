075a9621a6a3dae4d64619f3317b9baa195cdfc0
Matyáš Volf <volfmatyas1@gmail.com>
2025-08-19 11:46:25 +0200

    Start development on agi theme

--- src/components/theme/switch-theme-button.svelte
+++ src/components/theme/switch-theme-button.svelte
@@ -4,8 +4,10 @@
 	import { onMount } from 'svelte';
 	import { on } from 'svelte/events';
 
+	type ThemeColor = 'light' | 'dark' | 'colorful';
+
 	// eslint-disable-next-line svelte/prefer-writable-derived
-	let currentTheme: 'light' | 'dark' = $state('light');
+	let currentTheme: ThemeColor = $state('colorful'); // Temporary for colorful theme development
 
 	onMount(() => {
 		const mediaQuery = matchMedia('(prefers-color-scheme: dark)');
@@ -16,7 +18,7 @@
 		});
 
 		$effect(() => {
-			currentTheme = (localStorage.getItem('color-theme') as 'dark' | 'light') ?? preferredTheme;
+			currentTheme = (localStorage.getItem('color-theme') as ThemeColor) ?? preferredTheme;
 		});
 
 		return removeMediaQueryListener;
