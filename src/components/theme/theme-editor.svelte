b1da5e0931e6b722425f42cb9391ba1c448454a0
Matyáš Volf <volfmatyas1@gmail.com>
2026-01-17 23:05:07 +0100

    Hack together an import/export system

--- src/components/theme/theme-editor.svelte
+++ src/components/theme/theme-editor.svelte
@@ -34,45 +34,123 @@
 			})
 		);
 	});
+
+	function exportData() {
+		const localStorageContents = Object.fromEntries(
+			[
+				'color-theme',
+				'custom-colors',
+				'filled-examples',
+				'link-storage_read',
+				'link-storage_unread'
+			].map((key) => {
+				const value = localStorage.getItem(key);
+				return [key, value];
+			})
+		);
+
+		const blob = new Blob([JSON.stringify(localStorageContents, null, 2)], {
+			type: 'application/json'
+		});
+
+		const objectURL = URL.createObjectURL(blob);
+		try {
+			const a = document.createElement('a');
+			a.download = 'reading-list-data.json';
+			a.href = objectURL;
+			a.click();
+		} finally {
+			URL.revokeObjectURL(objectURL);
+		}
+	}
+
+	async function importData() {
+		const input = document.createElement('input') as HTMLInputElement;
+		input.type = 'file';
+
+		const filePromise = new Promise((resolve) => {
+			input.addEventListener(
+				'change',
+				() => {
+					resolve(input.files?.[0]);
+				},
+				{ once: true }
+			);
+		});
+		input.click();
+
+		const file = (await filePromise) as File | undefined;
+
+		if (!file) {
+			throw new Error('No file contents :(');
+		}
+
+		const fileContents = await file.text();
+		const localStorageContents = JSON.parse(fileContents);
+		for (const [key, value] of Object.entries(localStorageContents)) {
+			if (typeof key !== 'string' || typeof value !== 'string') {
+				throw new Error('Bad data');
+			}
+		}
+
+		localStorage.clear();
+
+		for (const [key, value] of Object.entries(localStorageContents)) {
+			localStorage.setItem(key, value as string);
+		}
+
+		location.reload();
+	}
 </script>
 
-<menu class="theme-editor">
-	<li>
-		<label>
-			<span class="label">Text</span>
-			<input type="text" placeholder={textDefault} bind:value={text.value} />
-			<input type="color" bind:value={text.value} />
-		</label>
-	</li>
-	<li>
-		<label>
-			<span class="label">Background</span>
-			<input type="text" placeholder={backgroundDefault} bind:value={background.value} />
-			<input type="color" bind:value={background.value} />
-		</label>
-	</li>
-	<li>
-		<label>
-			<span class="label">Background (alternative)</span>
-			<input type="text" placeholder="#c0ffee" bind:value={backgroundAlt.value} />
-			<input
-				type="color"
-				oninput={function () {
-					backgroundAlt.value = this.value;
-				}}
-			/>
-		</label>
-	</li>
-	<li>
-		<label>
-			<span class="label">Background (accent)</span>
-			<input type="text" placeholder="#c0ffee" bind:value={backgroundAccent.value} />
-			<input
-				type="color"
-				oninput={function () {
-					backgroundAccent.value = this.value;
-				}}
-			/>
-		</label>
-	</li>
-</menu>
+<div class="theme-editor">
+	<menu>
+		<li>
+			<label>
+				<span class="label">Text</span>
+				<input type="text" placeholder={textDefault} bind:value={text.value} />
+				<input type="color" bind:value={text.value} />
+			</label>
+		</li>
+		<li>
+			<label>
+				<span class="label">Background</span>
+				<input type="text" placeholder={backgroundDefault} bind:value={background.value} />
+				<input type="color" bind:value={background.value} />
+			</label>
+		</li>
+		<li>
+			<label>
+				<span class="label">Background (alternative)</span>
+				<input type="text" placeholder="#c0ffee" bind:value={backgroundAlt.value} />
+				<input
+					type="color"
+					oninput={function () {
+						backgroundAlt.value = this.value;
+					}}
+				/>
+			</label>
+		</li>
+		<li>
+			<label>
+				<span class="label">Background (accent)</span>
+				<input type="text" placeholder="#c0ffee" bind:value={backgroundAccent.value} />
+				<input
+					type="color"
+					oninput={function () {
+						backgroundAccent.value = this.value;
+					}}
+				/>
+			</label>
+		</li>
+	</menu>
+
+	<menu class="buttons">
+		<li>
+			<button type="button" onclick={importData}>Import data</button>
+		</li>
+		<li>
+			<button type="button" onclick={exportData}>Export data</button>
+		</li>
+	</menu>
+</div>
